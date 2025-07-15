87f05c9cc5b3a22be73e2b320700620d9b009830
dgkim0329 <dgkim0329@gmail.com>
2025-07-15 14:24:48 +0900

    Update 2d.js

--- 2d.js
+++ 2d.js
@@ -151,7 +151,7 @@ function sList(lat, siderealTime, off, starEq, missingStarCount, limitingMagnitu
         }
     });
 
-    const brightStarsToRemove = starsAboveFiveDegrees.filter(star => star.mag <= limitingMagnitude - 1);
+    const brightStarsToRemove = starsAboveFiveDegrees.filter(star => star.mag <= 1.3);
 
     for (let i = 0; i < missingStarCount && brightStarsToRemove.length > 0; i++) {
         const randomIndex = Math.floor(Math.random() * brightStarsToRemove.length);
